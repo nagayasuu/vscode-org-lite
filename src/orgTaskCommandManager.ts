2598545ca7799c481d5680d83bf0ecbd8a17f0f2
nagayasuu <ynagayasu1@gmail.com>
2025-08-16 15:04:58 +0900

    refactor orgTaskCommandManager

--- src/orgTaskCommandManager.ts
+++ src/orgTaskCommandManager.ts
@@ -1,31 +1,23 @@
 import * as vscode from 'vscode';
 import * as taskCommands from './commands/orgTaskCommands';
 
-/**
- * TODO Rotation functionality for Org mode files
- */
 export class OrgTaskCommandManager {
-  /**
-   * Register TODO rotation commands
-   */
   public static registerCommands(context: vscode.ExtensionContext): void {
-    // Rotate TODO command (forward)
-    const rotateTodoDisposable = vscode.commands.registerCommand(
-      'org-lite.rotateTodo',
-      () => {
-        taskCommands.rotateTodoState(false);
-      }
-    );
+    const commandRegistrations = [
+      {
+        command: 'org-lite.rotateTodo',
+        callback: () => taskCommands.rotateTodoState(false),
+      },
+      {
+        command: 'org-lite.rotateTodoReverse',
+        callback: () => taskCommands.rotateTodoState(true),
+      },
+    ];
 
-    // Rotate TODO command (reverse)
-    const rotateTodoReverseDisposable = vscode.commands.registerCommand(
-      'org-lite.rotateTodoReverse',
-      () => {
-        taskCommands.rotateTodoState(true);
-      }
-    );
-
-    context.subscriptions.push(rotateTodoDisposable);
-    context.subscriptions.push(rotateTodoReverseDisposable);
+    for (const { command, callback } of commandRegistrations) {
+      context.subscriptions.push(
+        vscode.commands.registerCommand(command, async () => callback())
+      );
+    }
   }
 }
